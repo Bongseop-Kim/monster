773fa05d46d627e594f5c1f8bd2e1fd8b2701999
Bongseop-Kim <96857444+Bongseop-Kim@users.noreply.github.com>
2023-03-30 17:59:05 +0900

    swagger 작성

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -9,6 +9,7 @@ import {
   UseFilters,
   UseInterceptors,
 } from '@nestjs/common';
+import { ApiOperation } from '@nestjs/swagger';
 import { HttpExceptionFilter } from 'src/common/exception/http-exception.filter';
 import { SuccessInterceptor } from 'src/common/interceptor/success.interceptor';
 import { CommentsService } from './comments.service';
@@ -19,6 +20,9 @@ import { CommentsService } from './comments.service';
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
+  @ApiOperation({
+    summary: '댓글 작성하기',
+  })
   @Post(':postId/:userId')
   async createPostLikes(
     @Param('postId') postId: number,
@@ -29,12 +33,18 @@ export class CommentsController {
   }
 
   //해당 게시물의 모든 댓글 가져오기
+  @ApiOperation({
+    summary: '해당 게시물의 모든 댓글 가져오기',
+  })
   @Get(':postId')
   async getAllComment(@Param('postId') postId: number) {
     const comments = await this.commentsService.getAllComment(postId);
     return comments;
   }
 
+  @ApiOperation({
+    summary: '댓글 수정하기',
+  })
   @Patch(':commentId')
   async updateComment(@Param('commentId') commentId: number, @Body() body) {
     const updatedComment = await this.commentsService.updateComment(
@@ -44,6 +54,9 @@ export class CommentsController {
     return updatedComment;
   }
 
+  @ApiOperation({
+    summary: '댓글 삭제하기',
+  })
   @Delete(':commentId')
   async deleteComment(@Param('commentId') commentId: number) {
     await this.commentsService.deleteComment(commentId);
